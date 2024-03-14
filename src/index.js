22dd79a89f68e0d700f3fa2923fe9adb135591ff
Tan Yi Jun <tanyijun6@gmail.com>
2024-03-14 16:39:11 +0800

    Add get weather info function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+const API_KEY = '85e15c7e15584e2ba2e160829241103';
+
+async function getWeatherInfo(location = 'Singapore') {
+  try {
+    const address = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`;
+    const response = await fetch(address, { mode: 'cors' });
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+console.log(await getWeatherInfo('london'));
