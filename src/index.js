4e05296fe53b94967a1fad926843b25afb3c09ca
Tan Yi Jun <tanyijun6@gmail.com>
2024-03-14 21:42:06 +0800

    Create weather object

--- src/index.js
+++ src/index.js
@@ -13,7 +13,33 @@ async function getCurrentWeatherData(location = 'Singapore') {
   }
 }
 
+function createWeather(data) {
+  const { current } = data;
+
+  return {
+    getLastUpdated: () => current.last_updated,
+    getCondition: () => current.condition.text,
+    getConditionIcon: () => current.condition.icon,
+    getTempC: () => current.temp_c,
+    getTempF: () => current.temp_f,
+    getFeelsLikeC: () => current.feelslike_c,
+    getFeelsLikeF: () => current.feelslike_f,
+    getAirQuality: () => current.air_quality['us-epa-index'],
+    getWindSpeedKPH: () => current.wind_kph,
+    getWindSpeedMPH: () => current.wind_mph,
+    getHumidity: () => current.humidity,
+    getVisibilityKM: () => current.vis_km,
+    getVisibilityMiles: () => current.vis_miles,
+    getPressureMb: () => current.pressure_mb,
+    getPressureIn: () => current.pressure_in,
+    getUV: () => current.uv,
+    isDay: () => current.is_day,
+  };
+}
+
 const location = 'Singapore';
 const currentWeatherData = await getCurrentWeatherData(location);
+const weather = createWeather(currentWeatherData);
 
 console.log(currentWeatherData);
+console.log(weather.getAirQuality());
