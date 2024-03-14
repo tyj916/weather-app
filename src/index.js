c0acc3ea6a31c39c1f9f22f26bc8a28af4944d58
Tan Yi Jun <tanyijun6@gmail.com>
2024-03-14 21:12:27 +0800

    Add comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 const API_KEY = '85e15c7e15584e2ba2e160829241103';
 
+// eslint-disable-next-line consistent-return
 async function getCurrentWeatherInfo(location = 'Singapore') {
   try {
     const address = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=yes`;
