73d6cd8b7c1ec2789af61abb58869194d50971d3
Tan Yi Jun <tanyijun6@gmail.com>
2024-03-18 17:03:58 +0800

    Add more render data

--- src/index.js
+++ src/index.js
@@ -19,38 +19,138 @@ function createWeather(data) {
 
   const dataContainer = document.querySelector('#weather-data');
 
+  function getAirQuality() {
+    const airQualityChart = [
+      'Good',
+      'Moderate',
+      'Unhealthy for Sensitive Group',
+      'Unhealthy',
+      'Very Unhealthy',
+      'Hazardous',
+    ];
+    const currentAirQuality = current.air_quality['us-epa-index'];
+    return airQualityChart[currentAirQuality - 1];
+  }
+
+  function getUVValue() {
+    const currentUVIndex = current.uv;
+
+    if (currentUVIndex <= 2) {
+      return 'Low';
+    }
+    if (currentUVIndex <= 5) {
+      return 'Moderate';
+    }
+    if (currentUVIndex <= 7) {
+      return 'High';
+    }
+    if (currentUVIndex <= 10) {
+      return 'Very High';
+    }
+    return 'Extreme';
+  }
+
   function render() {
     dataContainer.textContent = '';
 
     const mainContainer = document.createElement('div');
     const locationEl = document.createElement('h2');
+    const lastUpdateEl = document.createElement('p');
+
+    const weatherConditionContainer = document.createElement('div');
+    const weatherIconEl = document.createElement('img');
+    const tempEl = document.createElement('p');
+    const conditionEl = document.createElement('p');
+    const feelsLikeEl = document.createElement('p');
+
+    const subContainer = document.createElement('div');
+    const airQualityContainer = document.createElement('div');
+    const airQualityLabel = document.createElement('p');
+    const airQualityEl = document.createElement('p');
+    const windContainer = document.createElement('div');
+    const windLabel = document.createElement('p');
+    const windEl = document.createElement('p');
+    const humidityContainer = document.createElement('div');
+    const humidityLabel = document.createElement('p');
+    const humidityEl = document.createElement('p');
+    const visibilityContainer = document.createElement('div');
+    const visibilityLabel = document.createElement('p');
+    const visibilityEl = document.createElement('p');
+    const pressureContainer = document.createElement('div');
+    const pressureLabel = document.createElement('p');
+    const pressureEl = document.createElement('p');
+    const UVContainer = document.createElement('div');
+    const UVLabel = document.createElement('p');
+    const UVEl = document.createElement('p');
 
     mainContainer.id = 'main-data';
+    weatherConditionContainer.id = 'weather-condition-container';
+    weatherIconEl.id = 'weather-icon';
+    tempEl.id = 'temp';
+    conditionEl.id = 'weather-condition';
+    feelsLikeEl.id = 'feelslike';
 
-    locationEl.textContent = location.name;
+    subContainer.id = 'sub-data';
+    airQualityContainer.className = 'data-container';
+    windContainer.className = 'data-container';
+    humidityContainer.className = 'data-container';
+    visibilityContainer.className = 'data-container';
+    pressureContainer.className = 'data-container';
+    UVContainer.className = 'data-container';
+
+    locationEl.textContent = location.region
+      ? `${location.name}, ${location.region}, ${location.country}`
+      : `${location.name}, ${location.country}`;
+    lastUpdateEl.textContent = `Last updated: ${current.last_updated}`;
+    tempEl.textContent = `${current.temp_c}°C`;
+    conditionEl.textContent = current.condition.text;
+    feelsLikeEl.textContent = current.feelslike_c;
+    airQualityLabel.textContent = 'Air Quality';
+    airQualityEl.textContent = getAirQuality();
+    windLabel.textContent = 'Wind';
+    windEl.textContent = `${current.wind_kph} km/h`;
+    humidityLabel.textContent = 'Humidity';
+    humidityEl.textContent = `${current.humidity}%`;
+    visibilityLabel.textContent = 'Visibility';
+    visibilityEl.textContent = `${current.vis_km} km`;
+    pressureLabel.textContent = 'Pressure';
+    pressureEl.textContent = `${current.pressure_mb} mb`;
+    UVLabel.textContent = `UV Index`;
+    UVEl.textContent = `${current.uv} - ${getUVValue()}`;
+
+    weatherIconEl.src = current.condition.icon;
+    weatherIconEl.alt = 'Weather condition icon';
 
     dataContainer.appendChild(mainContainer);
+    dataContainer.appendChild(subContainer);
     mainContainer.appendChild(locationEl);
+    mainContainer.appendChild(lastUpdateEl);
+    mainContainer.appendChild(weatherConditionContainer);
+    weatherConditionContainer.appendChild(weatherIconEl);
+    weatherConditionContainer.appendChild(tempEl);
+    weatherConditionContainer.appendChild(conditionEl);
+    weatherConditionContainer.appendChild(feelsLikeEl);
+    subContainer.appendChild(airQualityContainer);
+    airQualityContainer.appendChild(airQualityLabel);
+    airQualityContainer.appendChild(airQualityEl);
+    subContainer.appendChild(windContainer);
+    windContainer.appendChild(windLabel);
+    windContainer.appendChild(windEl);
+    subContainer.appendChild(humidityContainer);
+    humidityContainer.appendChild(humidityLabel);
+    humidityContainer.appendChild(humidityEl);
+    subContainer.appendChild(visibilityContainer);
+    visibilityContainer.appendChild(visibilityLabel);
+    visibilityContainer.appendChild(visibilityEl);
+    subContainer.appendChild(pressureContainer);
+    pressureContainer.appendChild(pressureLabel);
+    pressureContainer.appendChild(pressureEl);
+    subContainer.appendChild(UVContainer);
+    UVContainer.appendChild(UVLabel);
+    UVContainer.appendChild(UVEl);
   }
 
   return {
-    getLastUpdated: () => current.last_updated,
-    getCondition: () => current.condition.text,
-    getConditionIcon: () => current.condition.icon,
-    getTempC: () => current.temp_c,
-    getTempF: () => current.temp_f,
-    getFeelsLikeC: () => current.feelslike_c,
-    getFeelsLikeF: () => current.feelslike_f,
-    getAirQuality: () => current.air_quality['us-epa-index'],
-    getWindSpeedKPH: () => current.wind_kph,
-    getWindSpeedMPH: () => current.wind_mph,
-    getHumidity: () => current.humidity,
-    getVisibilityKM: () => current.vis_km,
-    getVisibilityMiles: () => current.vis_miles,
-    getPressureMb: () => current.pressure_mb,
-    getPressureIn: () => current.pressure_in,
-    getUV: () => current.uv,
-    isDay: () => current.is_day,
     render,
   };
 }
@@ -71,6 +171,7 @@ searchbar.type = 'searchbar';
 submitBtn.type = 'submit';
 
 searchbar.placeholder = 'Search for location...';
+searchbar.required = true;
 
 submitBtn.textContent = 'Search';
 
@@ -84,15 +185,15 @@ locationForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const response = getCurrentWeatherData(searchbar.value);
-  response.then((data) => {
-    const weather = createWeather(data);
-    weather.render();
-  });
+  response
+    .then((data) => {
+      const weather = createWeather(data);
+      weather.render();
+    })
+    .catch((err) => console.error(err));
 });
 
-const location = 'London';
+const location = 'Thailand';
 const currentWeatherData = await getCurrentWeatherData(location);
 const weather = createWeather(currentWeatherData);
-
-console.log(currentWeatherData);
 weather.render();
